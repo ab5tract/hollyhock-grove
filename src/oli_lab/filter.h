15d504e1253e9dd3bf0ca3b630b0a552c12e4038
Oli-Lab <Oli-Lab@users.noreply.github.com>
2016-02-23 00:54:04 +0100

    new multitap delay line

---
+++ src/oli_lab/filter.h
@@ -0,0 +1,161 @@
+//-----------------------------------------------------------------------------
+//@file  
+//	multitap delay.h
+//
+//  Oli_Lab with precious help from
+//	Martin FLEURENT aka 'martignasse'
+//
+//@brief 
+//	Implementation of the multitap delay class.
+//
+//  a delay line with up to 64 taps !
+//
+//@historic 
+//	2015/02/23
+//    first release for Hollyhock CPP SDK 6.04.001
+//
+//@IMPORTANT
+//	This file is part of the Usine Hollyhock CPP SDK
+//
+//  Please, report bugs and patch to Usine forum :
+//  http://www.sensomusic.com/wiki2/doku.php?id=hollyhock:bugsreport 
+//
+// All dependencies are under there own licence.
+//
+//@LICENCE
+// Copyright (C) 2013, 2014, 2015 Sensomusic
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of 
+// this software and associated documentation files (the "Software"), 
+// to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software 
+// is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all 
+//     copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE.
+//
+//-----------------------------------------------------------------------------
+
+// include once, no more
+#ifndef __ALLPASS_F_H__
+#define __ALLPASS_F_H__
+
+//-----------------------------------------------------------------------------
+// includes
+//-----------------------------------------------------------------------------
+#include "../../sdk/UserDefinitions.h"  
+//#include "delay.hpp"
+//#include "allpass.hpp"
+
+//-----------------------------------------------------------------------------
+// defines and constantes
+//-----------------------------------------------------------------------------
+#define AUDIO_INS_OUTS_MAX 64
+const float SMOOTH = 0.99f;
+//const float SMOOTH_FAST = 0.11f;
+//-----------------------------------------------------------------------------
+// class definition
+//-----------------------------------------------------------------------------
+class filter : public UserModuleBase
+{
+	//-------------------------------------------------------------------------
+	// module constructors/destructors
+	//-------------------------------------------------------------------------
+public:
+	// constructor
+	filter();
+
+	// destructor
+	virtual ~filter();
+
+	//-------------------------------------------------------------------------
+	// public methodes inherited from UserModule
+	//-------------------------------------------------------------------------
+public:
+	//-----------------------------------------------------------------------------
+	// module info
+	void onGetModuleInfo(MasterInfo* pMasterInfo, ModuleInfo* pModuleInfo);
+
+	//-----------------------------------------------------------------------------
+	// query system and init
+	int  onGetNumberOfParams(int queryIndex);
+	void onAfterQuery(MasterInfo* pMasterInfo, ModuleInfo* pModuleInfo, int queryIndex);
+	void onInitModule(MasterInfo* pMasterInfo, ModuleInfo* pModuleInfo);
+
+	//-----------------------------------------------------------------------------
+	// parameters and process
+	void onGetParamInfo(int ParamIndex, TParamInfo* pParamInfo);
+	void onSetEventAddress(int ParamIndex, UsineEventPtr pEvent);
+	void onCallBack(UsineMessage *Message);
+	void onProcess();
+	
+	//-----------------------------------------------------------------------------
+	// audio setup update
+	void onBlocSizeChange(int BlocSize);
+	void onSampleRateChange(double SampleRate);
+
+	//-----------------------------------------------------------------------------
+	//Process one sample	
+	//void Process(TPrecision* sample);// Address to store processed sample value
+	//Get sample delayed
+	TPrecision GetDelayedSample(int delay, int cursor);	// Delay expressed in samples
+	//Reads a sample
+	//TPrecision ReadSample(int i);
+	//Writes a sample
+	void  WriteSample(TPrecision sample);
+
+
+
+
+	//-------------------------------------------------------------------------
+	// private members
+	//-------------------------------------------------------------------------
+private:
+	//float map(float x, float in_min, float in_max, float out_min, float out_max);
+	//-------------------------------------------------------------------------
+	// parameters events
+	
+	UsineEventPtr audioInputs;     // audio input
+	UsineEventPtr audioOutputs[AUDIO_INS_OUTS_MAX];    // audio output
+	UsineEventPtr speed[AUDIO_INS_OUTS_MAX];
+	UsineEventPtr length;
+	UsineEventPtr positions;
+	UsineEventPtr getPositions;
+	//-------------------------------------------------------------------------
+	int m_size;			// Size of delay in samples
+	int max_size;
+	int m_read[AUDIO_INS_OUTS_MAX];			// Read position
+	TPrecision m_positions[AUDIO_INS_OUTS_MAX];			// Head positions
+	float m_speed[AUDIO_INS_OUTS_MAX];
+	TPrecision tapOutOld[AUDIO_INS_OUTS_MAX];
+	//TPrecision Ynm1[AUDIO_INS_OUTS_MAX];
+	//float m_delayInt[AUDIO_INS_OUTS_MAX];
+	//TPrecision m_delayOld;
+	float m_delaySmoothed[AUDIO_INS_OUTS_MAX];
+	float m_delayFinal[AUDIO_INS_OUTS_MAX];
+	int m_write;		// Write position, always will be (read + size) % size
+	
+	
+	TPrecision* m_buffer;	// Samples buffer
+	const int lineSize = 120; // secondes
+	//long int sampleSize;
+	int SampleRate;
+	int blocSize;
+	//-------------------------------------------------------------------------
+	static const int numOfParamAfterAudiotInOut = 3;
+
+	int queryIndex;
+	int numOfAudiotInsOuts;
+
+}; // class 
+
+#endif //__
